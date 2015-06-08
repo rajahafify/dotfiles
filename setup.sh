219c8159c5a3c2bc56ff19176d599f3ac9153364
Raja Hafify <rajahafify.zaed@gmail.com>
2015-06-08 16:08:31 +0800

    Installing RVM in zsh

--- setup.sh
+++ setup.sh
@@ -51,4 +51,4 @@ brew install gnupg gnupg2
 
 echo "Installing rvm"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+\curl -sSL https://get.rvm.io | zsh -s stable --ruby
